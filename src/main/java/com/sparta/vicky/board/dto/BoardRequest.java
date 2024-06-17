673dc37f2bd1c71013ee132a3379b239ef68dadf
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-06-17 15:36:44 +0900

    Refactor: BoardRequest DTO 리팩토링

--- src/main/java/com/sparta/vicky/board/dto/BoardRequest.java
+++ src/main/java/com/sparta/vicky/board/dto/BoardRequest.java
@@ -2,20 +2,14 @@ package com.sparta.vicky.board.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
 @Builder
 public class BoardRequest {
 
     @NotBlank
     private String title;
-
-    @NotBlank
-    private String region;
-
-    @NotBlank
-    private String address;
 
     @NotBlank
     private String content;
