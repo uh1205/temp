8b755a5afeaaec7ed915090013e04eeed5a27a6b
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-06-17 10:38:15 +0900

    Test: BoardService updateBoard() 테스트

---
+++ src/test/java/com/sparta/vicky/board/service/BoardServiceTest.java
@@ -0,0 +1,67 @@
+package com.sparta.vicky.board.service;
+
+import com.sparta.vicky.board.dto.BoardRequest;
+import com.sparta.vicky.board.entity.Board;
+import com.sparta.vicky.board.repository.BoardRepository;
+import com.sparta.vicky.user.entity.User;
+import com.sparta.vicky.user.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class BoardServiceTest {
+
+    @Mock
+    BoardRepository boardRepository;
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    User user;
+
+    @Test
+    void updateBoard() {
+        // given
+        Long userId = 1L;
+        Long boardId = 1L;
+
+        BoardRequest createRequest = BoardRequest.builder()
+                .title("title")
+                .content("content")
+                .address("address")
+                .region("region")
+                .build();
+
+        BoardRequest updateRequest = BoardRequest.builder()
+                .title("newTitle")
+                .content("newContent")
+                .address("newAddress")
+                .region("newRegion")
+                .build();
+
+        Board board = new Board(createRequest, user);
+
+        given(board.getUser().getId()).willReturn(userId);
+        given(boardRepository.findById(boardId)).willReturn(Optional.of(board));
+
+        BoardService boardService = new BoardService(boardRepository, userService);
+
+        // when
+        Board newBoard = boardService.updateBoard(boardId, updateRequest, user);
+
+        // then
+        assertEquals("newTitle", newBoard.getTitle());
+        assertEquals("newContent", newBoard.getContent());
+        assertEquals("newAddress", newBoard.getAddress());
+        assertEquals("newRegion", newBoard.getRegion());
+    }
+
+}
